8a44056bbcee73abb3e87eb6a512f9788a57d723
Gibsonzwenyika <gzwenyika@gmail.com>
2021-06-21 13:56:08 -0700

    added two files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main-diplay
+ *Description: display alph
+ *Return: Always zero
+ */
+int main (void)
+{
+int ch;
+for (ch= ‘a’; ch<= ‘z’; ch++)
+putchar(ch);
+putchar(‘\n’) ;
+return (0);
+}
