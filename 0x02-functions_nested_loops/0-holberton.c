ff7847686575f9533e36fbcfe92aa5a5ebc03195
Gibsonzwenyika <gzwenyika@gmail.com>
2021-06-22 12:15:37 -0700

    added two files

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main- display
+ *Desription: diplay name
+ *Return: always zero
+ */
+int main(void)
+{
+char ch = 'Holberton';
+putchar(ch);
+putchar(\n);
+return (0);
+}
